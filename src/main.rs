2b9e25189a85c8c820464a6415c916b45343eeaf
Kevin Trieu <kevin.trieu5813@gmail.com>
2022-04-30 00:45:26 -0400

    Add a bunch of stuff to get our UEFI app building, ignore folders

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+#![no_main]
+#![no_std]
+#![feature(abi_efiapi)]
+
+use core::fmt::Write;
+
+use uefi::prelude::*;
+
+#[entry]
+fn uefi_main(_handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
+    uefi_services::init(&mut system_table).unwrap();
+
+    system_table
+        .stdout()
+        .write_str("Hello from ugoOS!")
+        .unwrap();
+
+    loop {}
 }
