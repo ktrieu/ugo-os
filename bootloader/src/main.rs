704586b7d6ff56280a2616149550f847fd9664fa
Kevin Trieu <kevin.trieu5813@gmail.com>
2023-01-03 00:31:46 -0500

    Let's start from scratch on the ELF loading

--- bootloader/src/main.rs
+++ bootloader/src/main.rs
@@ -2,31 +2,19 @@
 #![no_std]
 #![feature(abi_efiapi)]
 
-use core::fmt::Write;
 use core::panic::PanicInfo;
 use core::slice;
 
-use common::KMEM_START;
-use common::PAGE_SIZE;
-use loader::load_kernel;
-use logger::LOGGER;
-use mem::valloc::VirtualAllocator;
 use uefi::prelude::*;
+
+#[macro_use]
+mod logger;
 
 mod fs;
 mod graphics;
-mod loader;
-#[macro_use]
-mod logger;
-mod mem;
 
-use mem::frame::FrameAllocator;
-use uefi::table::boot::MemoryDescriptor;
 use uefi::table::boot::MemoryMapSize;
 use uefi::table::boot::MemoryType;
-use x86_64::structures::paging::OffsetPageTable;
-use x86_64::structures::paging::PageTable;
-use x86_64::VirtAddr;
 
 fn get_memory_map_size(boot_services: &BootServices) -> usize {
     let MemoryMapSize {
@@ -37,32 +25,6 @@ fn get_memory_map_size(boot_services: &BootServices) -> usize {
     map_size += 2;
 
     entry_size * map_size
-}
-
-fn create_page_table<'a, I>(
-    frame: &mut FrameAllocator<'a, I>,
-    virt: &mut VirtualAllocator,
-) -> (VirtAddr, OffsetPageTable<'static>, u64)
-where
-    I: ExactSizeIterator<Item = &'a MemoryDescriptor> + Clone,
-{
-    let num_frames = frame.total_physical_memory() / PAGE_SIZE;
-    // Probably rework this later, but map all of our physical memory right at the start of virtual memory
-    let phys_mem_offset = VirtAddr::new(
-        virt.allocate(num_frames)
-            .expect("Could not allocate virtual memory space for physical memory mapping."),
-    );
-
-    let new_frame = frame
-        .allocate(1)
-        .expect("No free frames for kernel page table.");
-
-    unsafe {
-        let page_ptr = new_frame as *mut PageTable;
-        *page_ptr = PageTable::new();
-        let page_table = OffsetPageTable::new(&mut *page_ptr, phys_mem_offset);
-        (phys_mem_offset, page_table, new_frame)
-    }
 }
 
 #[panic_handler]
@@ -105,43 +67,9 @@ fn uefi_main(handle: Handle, system_table: SystemTable<Boot>) -> Status {
         slice::from_raw_parts_mut(raw_buffer, mem_map_buffer_size)
     };
 
-    let (_, descriptors) = system_table
+    let (_, _descriptors) = system_table
         .exit_boot_services(handle, mem_map_buffer)
         .expect("Could not exit boot services.");
-
-    let mut frame = FrameAllocator::new(descriptors.clone());
-    bootlog!(
-        "{} bytes of memory detected.",
-        frame.total_physical_memory()
-    );
-
-    bootlog!("Free memory segments:");
-    for d in descriptors
-        .clone()
-        .filter(|d| d.ty == MemoryType::CONVENTIONAL)
-    {
-        bootlog!(
-            "{:#x} - {:#x} ({} pages)",
-            d.phys_start,
-            d.phys_start + (PAGE_SIZE * d.page_count),
-            d.page_count
-        );
-    }
-
-    let mut virt = VirtualAllocator::new(KMEM_START);
-
-    let (phys_mem_offset, mut page_table, page_table_addr) =
-        create_page_table(&mut frame, &mut virt);
-
-    bootlog!("Mapping physical memory starting at {:#x}", phys_mem_offset);
-    bootlog!("Creating kernel page table at {:#x}", page_table_addr);
-
-    load_kernel(file, &mut frame, &mut virt, &mut page_table).expect("Failed to load kernel!");
-
-    bootlog!(
-        "Kernel loaded.\nPage table:\n{:?}",
-        page_table.level_4_table().iter().find(|e| !e.is_unused())
-    );
 
     loop {}
 }
